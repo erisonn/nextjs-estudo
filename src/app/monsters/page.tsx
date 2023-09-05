2d05a900fafbd8ec16b6d745570c89e45fd30855
erisonn <erison.frontend@gmail.com>
2023-09-05 17:46:07 -0300

    fetch data on server

--- src/app/monsters/page.tsx
+++ src/app/monsters/page.tsx
@@ -1,4 +1,24 @@
-export default function Monsters() {
-    return <h1>Monsters</h1>;
+async function getData() {
+  const res = await fetch("https://mhw-db.com/monsters");
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
   }
-  
+  return res.json();
+}
+
+interface IMonster {
+  name: string
+  id: number
+}
+
+export default async function Monsters() {
+  const data = await getData();
+  return (
+    <div>
+      <h1>Monsters</h1>
+      {data?.map((monster: IMonster) => (
+        <p>{monster?.name}</p>
+      ))}
+    </div>
+  );
+}
